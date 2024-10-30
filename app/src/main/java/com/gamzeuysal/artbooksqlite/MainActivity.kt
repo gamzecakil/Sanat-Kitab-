e437c15b9df25f8dbec9872b1c077cabc28ca4b4
gamzecakil <gmzcakil@gmail.com>
2024-10-30 12:31:24 +0300

    menu olusturuldu.

--- app/src/main/java/com/gamzeuysal/artbooksqlite/MainActivity.kt
+++ app/src/main/java/com/gamzeuysal/artbooksqlite/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.gamzeuysal.artbooksqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.gamzeuysal.artbooksqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+    //menu layout
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    //menu item  clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_art_name)
+        {
+            //Art Activity geçiş
+            val intent = Intent(this@MainActivity,ArtActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
